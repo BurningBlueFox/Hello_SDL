a7d17f788eb218b0ff62277b7c72be3af699d64c
Thiago Gorgulho Leão <burningbluefox@gmail.com>
2020-09-26 16:35:46 -0300

    Loads bitmap image and binds to screen

--- Hello_SDL/Main.cpp
+++ Hello_SDL/Main.cpp
@@ -4,15 +4,24 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main(int argc, char* args[])
+
+bool Init();
+bool LoadMedia();
+void Close();
+
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+SDL_Surface* helloWorldBitmap = NULL;
+
+bool Init()
 {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
+	bool success = true;
 
 	//InitializeSDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
 	}
 	else
 	{
@@ -23,23 +32,64 @@ int main(int argc, char* args[])
 		if (window == NULL)
 		{
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
 		}
 		else
 		{
 			screenSurface = SDL_GetWindowSurface(window);
+		}
+	}
+	return success;
+}
 
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+bool LoadMedia()
+{
+	bool success = true;
+
+	helloWorldBitmap = SDL_LoadBMP("Assets/Graphics/hello_world.bmp");
+	if (helloWorldBitmap == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "Assets/Graphics/hello_world.bmp", SDL_GetError());
+		success = false;
+	}
+	return success;
+}
+
+void Close()
+{
+	SDL_FreeSurface(helloWorldBitmap);
+	helloWorldBitmap = NULL;
+
+	SDL_DestroyWindow(window);
+	window = NULL;
+
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+
+	if (!Init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		if (!LoadMedia())
+		{
+			printf("Failed to load media\n");
+		}
+		else
+		{
+			SDL_BlitSurface(helloWorldBitmap, NULL, screenSurface, NULL);
 
 			SDL_UpdateWindowSurface(window);
 
-			SDL_Delay(2000);
-
-			SDL_DestroyWindow(window);
-
-			SDL_Quit();
-			return 0;
+			SDL_Delay(5000);
 		}
-
-
 	}
+	
+	Close();
+
+	return 0;
 }
